82e2d3a28a26cbc583318c331c89362679db7def
Nastya <himeriya97@mgmail.com>
2023-09-20 12:14:55 +0300

    feat: add personal info component

--- cv-gen/src/app/modules/employees/components/employee-details/employee-details.component.ts
+++ cv-gen/src/app/modules/employees/components/employee-details/employee-details.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   OnInit,
   Input,
+  ChangeDetectorRef,
 } from '@angular/core';
 import { EmployeeInterface } from '../../../core/models/employee.model';
 import { Observable } from 'rxjs';
@@ -18,10 +19,24 @@ export class EmployeeDetailsComponent implements OnInit {
   @Input() id = '';
 
   public employee$!: Observable<EmployeeInterface | null>;
+  public showSaveMessage = false;
 
-  constructor(private employeeService: EmployeesService) {}
+  constructor(
+    private employeeService: EmployeesService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   public ngOnInit(): void {
     this.employee$ = this.employeeService.getEmployeeById(this.id);
+  }
+
+  public onUpdateEmployee(id: string): void {
+    this.employee$ = this.employeeService.getEmployeeById(id);
+    this.showSaveMessage = true;
+
+    setTimeout(() => {
+      this.showSaveMessage = false;
+      this.cdr.markForCheck();
+    }, 2000);
   }
 }
--- cv-gen/src/app/modules/employees/components/employee-info/employee-info.component.ts
+++ cv-gen/src/app/modules/employees/components/employee-info/employee-info.component.ts
@@ -2,12 +2,13 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnInit,
   ViewChildren,
   QueryList,
+  Output,
+  EventEmitter,
 } from '@angular/core';
 import { EmployeeInterface } from '../../../core/models/employee.model';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { EmployeesService } from '../../../core/services/employees.service';
 import { SkillsComponent } from '../../../shared/components/skills/skills.component';
 
@@ -17,22 +18,14 @@ import { SkillsComponent } from '../../../shared/components/skills/skills.compon
   styleUrls: ['./employee-info.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EmployeeInfoComponent implements OnInit {
+export class EmployeeInfoComponent {
   @Input() employee!: EmployeeInterface;
+  @Output() saveInfo = new EventEmitter<string>();
 
   @ViewChildren(SkillsComponent)
   private skillsComponents!: QueryList<SkillsComponent>;
 
-  public infoForm = this.fb.nonNullable.group({
-    firstName: ['Default', [Validators.required, Validators.minLength(2)]],
-    lastName: ['', [Validators.required, Validators.minLength(2)]],
-    email: [
-      '',
-      [Validators.required, Validators.minLength(2), Validators.email],
-    ],
-    department: ['', [Validators.required, Validators.minLength(2)]],
-    line: ['', [Validators.required, Validators.minLength(2)]],
-  });
+  public infoForm = this.fb.nonNullable.group({});
 
   public skillsOptions = ['Javascript', 'Angular']; //get from entities
   public langsOptions = ['English', 'Russian']; //get from entities
@@ -42,44 +35,15 @@ export class EmployeeInfoComponent implements OnInit {
     private employeeService: EmployeesService
   ) {}
 
-  public ngOnInit(): void {
-    this.addFormInitialValues();
-  }
-
-  public get firstName(): FormControl<string | null> {
-    return this.infoForm.controls.firstName;
-  }
-
-  public get lastName(): FormControl<string | null> {
-    return this.infoForm.controls.lastName;
-  }
-
-  public get email(): FormControl<string | null> {
-    return this.infoForm.controls.email;
-  }
-
-  public get department(): FormControl<string | null> {
-    return this.infoForm.controls.department;
-  }
-
-  public get line(): FormControl<string | null> {
-    return this.infoForm.controls.line;
-  }
-
-  private addFormInitialValues(): void {
-    this.firstName.setValue(this.employee.firstName);
-    this.lastName.setValue(this.employee.lastName);
-    this.email.setValue(this.employee.email);
-    this.department.setValue(this.employee.department);
-    this.line.setValue(this.employee.line);
-  }
-
   public onSubmit(): void {
     if (this.infoForm.valid) {
       this.employeeService.updateEmployee(
         this.infoForm.getRawValue() as Omit<EmployeeInterface, 'id' | 'cvsId'>,
         this.employee.id
       );
+
+      this.saveInfo.emit(this.employee.id);
+
       return;
     }
 
@@ -87,6 +51,7 @@ export class EmployeeInfoComponent implements OnInit {
   }
 
   public onCancel(): void {
+    this.infoForm.reset();
     if (this.skillsComponents) {
       this.skillsComponents.forEach(val => {
         val.reset();
