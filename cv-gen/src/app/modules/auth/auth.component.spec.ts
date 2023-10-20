524f57db4a33101f8bd3cd28478794d3fb92e1e2
Nastya <himeriya97@mgmail.com>
2023-10-20 13:37:26 +0300

    fix: create tests for couple components

--- cv-gen/src/app/modules/auth/auth.component.spec.ts
+++ cv-gen/src/app/modules/auth/auth.component.spec.ts
@@ -1,14 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter } from '@angular/core';
+import {
+  LangChangeEvent,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PrimeDesignModule } from '../prime-design/prime-design.module';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  const translateService = jasmine.createSpyObj<TranslateService>(
+    'translateService',
+    ['stream', 'get']
+  );
+  const translateServiceMock = {
+    currentLang: 'en',
+    onLangChange: new EventEmitter<LangChangeEvent>(),
+    use: translateService.get,
+    get: translateService.get.and.returnValue(of('')),
+    stream: translateService.stream.and.returnValue(of('')),
+    onTranslationChange: new EventEmitter(),
+    onDefaultLangChange: new EventEmitter(),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot(),
+        SharedModule,
+        ReactiveFormsModule,
+        PrimeDesignModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [AuthComponent],
+      providers: [
+        provideMockStore({}),
+        {
+          provide: TranslateService,
+          useValue: translateServiceMock,
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
--- cv-gen/src/app/modules/home/components/main-page/main-page.component.spec.ts
+++ cv-gen/src/app/modules/home/components/main-page/main-page.component.spec.ts
@@ -1,14 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainPageComponent } from './main-page.component';
+import { HeaderComponent } from '../header/header.component';
+import { NavigationComponent } from '../navigation/navigation.component';
+import {
+  LangChangeEvent,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { EventEmitter } from '@angular/core';
+import { of } from 'rxjs';
+import { CrumbsComponent } from '../crumbs/crumbs.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from '../login/login.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { PrimeDesignModule } from '../../../prime-design/prime-design.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
+  const translateService = jasmine.createSpyObj<TranslateService>(
+    'translateService',
+    ['get', 'stream']
+  );
+  const translateServiceMock = {
+    currentLang: 'en',
+    onLangChange: new EventEmitter<LangChangeEvent>(),
+    use: translateService.get,
+    get: translateService.get.and.returnValue(of('')),
+    stream: translateService.stream.and.returnValue(of('')),
+    onTranslationChange: new EventEmitter(),
+    onDefaultLangChange: new EventEmitter(),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MainPageComponent]
+      imports: [
+        TranslateModule.forRoot(),
+        RouterTestingModule,
+        SharedModule,
+        PrimeDesignModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [
+        MainPageComponent,
+        HeaderComponent,
+        NavigationComponent,
+        CrumbsComponent,
+        LoginComponent,
+      ],
+      providers: [
+        provideMockStore({}),
+        {
+          provide: TranslateService,
+          useValue: translateServiceMock,
+        },
+      ],
     });
     fixture = TestBed.createComponent(MainPageComponent);
     component = fixture.componentInstance;
